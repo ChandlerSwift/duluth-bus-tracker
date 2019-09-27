7bd323ca89d46ce24a3af6845d0233098bd64d34
Chandler Swift <chandler@chandlerswift.com>
2019-09-27 14:37:11 -0500

    Update server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const request = require('request');
 const ws = require('ws');
+const fetch = require('node-fetch');
 
-const UPDATE_INTERVAL_MS = 500;
+const UPDATE_INTERVAL_MS = 5000;
 const VEHICLE_POSITION_DATA_URL = 'http://duluthtransit.com/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.json';
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new ws.Server({ port: 8080 });
 
 let connections = [];
 
@@ -18,24 +18,22 @@ wss.on('connection', function connection(conn) {
 let buses = [];
 
 /** Checks for updated bus locations, and sends updates to all connected clients */
-function updateBusLocations() {
-	let vehicles = getVehicles();
-	let relevantVehicles = filterVehiclesByRoute(vehicles, [6, 13, 23]);
+async function updateBusLocations() {
+	const vehicles = await getVehicles();
+	const relevantVehicles = filterVehiclesByRoute(vehicles, ['6', '13', '23']);
 	for (let bus of relevantVehicles) {
-		if (magicMethodForBusThatHasChanged()) { // TODO
-			for (let conn of connections) {
-				const update = '' // TODO
-				conn.send(update);
-			}
+		console.log(`Sending update for bus ${bus}`);
+		for (let conn of connections) {
+			const update = '' // TODO
+			conn.send(update);
 		}
 	}
 }
 
 /** Downloads the location of all buses, returns as an array of buses */
-function getVehicles() {
-	request(VEHICLE_POSITION_DATA_URL, function(error, response, body) { 
-		vehicle_data = JSON.parse(body); 
-	});
+async function getVehicles() {
+	const response = await fetch(VEHICLE_POSITION_DATA_URL);
+	const vehicle_data = await response.json();
 	return vehicle_data.entity;
 }
 
@@ -43,13 +41,8 @@ function getVehicles() {
  *  to return only the buses on the route numbers given (note that at certain
  *  times this may be more than one bus per route)
  */
-function filterVehiclesByRoute(locations, routes) {
-
-}
-
-/** */
-function magicMethodForBusThatHasChanged(bus) {
-	return true;
+function filterVehiclesByRoute(vehicles, routes) {
+	return vehicles.filter(vehicle => routes.includes(vehicle.vehicle.trip.route_id));
 }
 
 setInterval(updateBusLocations, UPDATE_INTERVAL_MS);
