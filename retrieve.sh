14a7cabf03d04f4a83112b92288bc989e728b082
Chandler Swift <chandler@chandlerswift.com>
2019-10-16 14:39:56 -0500

    Remove obsolete code

--- retrieve.sh
+++
@@ -1,6 +0,0 @@
-wget -q http://www.duluthtransit.com/gtf/google_transit.zip
-rm -r data
-mkdir data
-unzip -qd data google_transit.zip
-rm google_transit.zip
-echo "Files downloaded to $(echo data/*)"
