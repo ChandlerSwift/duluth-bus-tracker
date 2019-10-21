ed5e4dd445d18bb86daf1128c2f330d9a096c4dc
Chandler Swift <chandler@chandlerswift.com>
2019-10-21 14:37:55 -0500

    Add API Consumer

---
+++ api_consumer/__init__.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def get_buses(routes: List[str]):
+    """
+    Returns a list of bus objects and their locations. The argument routes
+    must be strings because of routes like the "11" vs "11K".
+    """
+    pass
