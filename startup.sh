5d6ef373918df3710217811d32e71cd4db49cbbd
Chandler Swift <chandler@chandlerswift.com>
2019-11-15 23:53:24 -0600

    Add startup script

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run the server
+pipenv run python server.py > server.log 2>&1 &
+
+# Run the client
+DISPLAY=:0 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:5000/
