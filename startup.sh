1a747159a550fdd6336f30b4cc041f9b9146b2dc
Chandler Swift <chandler@chandlerswift.com>
2019-11-18 23:06:44 -0600

    Move server.py to app.py to make Flask happy

--- startup.sh
+++ startup.sh
@@ -2,7 +2,8 @@
 
 # Run the server. It must be run as root because we do direct memory mapping
 # in order to drive the LEDs, which requires elevated privileges.
-sudo pipenv run python server.py > server.log 2>&1 &
+sudo pipenv run python app.py > server.log 2>&1 &
+# or sudo pipenv run flask run [--host=0.0.0.0]
 
 # Run the client
 DISPLAY=:0 /usr/bin/chromium-browser --no-sandbox --noerrdialogs --disable-infobars --kiosk http://localhost:5000/
