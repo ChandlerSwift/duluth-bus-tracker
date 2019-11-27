8961816f58fff891a17c3cba3685136e32cd3199
Chandler Swift <chandler@chandlerswift.com>
2019-11-27 15:34:01 -0600

    Improve route generator

--- util/generate_routes.sh
+++ util/generate_routes.sh
@@ -42,7 +42,7 @@ for route in 6 11 11K 13 23; do
     echo "    Parsing route $route..."
     sqlite3 database.sqlite >> tmp_stop_data <<EOF
 .mode csv
-select stop_name,stop_id,stop_lat,stop_lon,'$route' as route from stops where stop_id in (
+select '$route' as route,stop_name,stop_id,stop_lat,stop_lon from stops where stop_id in (
     select stop_id from stop_times where trip_id = (
         select trip_id from trips where route_id = '$route' LIMIT 1
     )
@@ -51,7 +51,7 @@ EOF
 done
 echo "Parsing complete!"
 
-echo "name,id,lat,long,route" > stop_data # create header row
+echo "route,name,id,lat,long" > stop_data # create header row
 
 echo "Filtering duplicate routes..."
 cat tmp_stop_data | sort | uniq >> stop_data
@@ -69,13 +69,17 @@ cat > json_generator.py <<EOF
 import csv
 import json
 
+filename=input("Where would you like to save the json file? ")
+
 with open('stop_data') as stop_data_file:
     reader = csv.DictReader(stop_data_file)
     stops = list(reader)
 
 for stop in stops:
     stop_led = input(stop['name'].ljust(20) + ": ")
-    stop['led'] = stop_led if stop_led is not "" else 0
+    stop['led'] = int(stop_led) if stop_led is not "" else 0
+    stop['lat'] = float(stop['lat'])
+    stop['long'] = float(stop['long'])
 
 with open('routes.txt') as routes_file:
     reader = csv.DictReader(routes_file)
@@ -91,11 +95,12 @@ for route in routes:
     for stop in stops:
         if stop['route'] == route['route_id']:
             output_routes[route['route_id']]['stops'].append(stop)
-print(json.dumps(output_routes))
+with open(filename, 'w') as f:
+ f.write(json.dumps(output_routes))
 EOF
 python3 json_generator.py
 
 echo "Cleaning up..."
-rm agency.txt calendar_dates.txt calendar.txt feed_info.txt \
+rm -f agency.txt calendar_dates.txt calendar.txt feed_info.txt \
     google_transit.zip routes.txt shapes.txt stop_times.txt stops.txt \
     trips.txt json_generator.py stop_data google.transit.zip.* database.sqlite
