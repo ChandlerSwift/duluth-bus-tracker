cdb4f3341f56d8488865784fce5de0750f8befc8
Chandler Swift <chandler@chandlerswift.com>
2019-11-28 13:47:00 -0600

    Update LED count

--- map_updater/display.py
+++ map_updater/display.py
@@ -1,7 +1,7 @@
 from rpi_ws281x import PixelStrip, Color
 from typing import Tuple
 
-LED_COUNT = 50        # Number of LED pixels.
+LED_COUNT = 117       # Number of LED pixels.
 LED_PIN = 12          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
